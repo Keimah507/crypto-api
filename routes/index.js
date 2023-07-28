92841045af40271223da9dc4bb7e3cedb88ce926
Keimah507 <githinjibrian5@gmail.com>
2023-07-28 20:55:31 +0300

    Add caching using redis

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = require('../server.js');
+const redisClient = require('../utils/redis.js');
 require('dotenv').config();
 
 
@@ -46,101 +47,71 @@ router.get('/crypto/dropdownprices', (req, res) => {
 
 router.get('/crypto/trendinglist', async(req, res) => {
     const trendinglist = [];
-    await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?convert=KES&limit=5', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
-        }
-    })
-    .then(response => response.json())
-    .then(async(data) => {
-        for (let i = 0; i < data.data.length; i++){
-            const item = data.data[i];
-            const { id, name, symbol } = item;
-            const {price, percent_change_24h} = item.quote.KES;
-            await fetch(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=${id}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
-                }
-            })
-            .then(response2 => response2.json())
-            .then(data2 => {
-                let logoUrl = data2.data[id].logo;
-                trendinglist.push({
-                    'name': name,
-                    'symbol': symbol,
-                    'price': price,
-                    'percent_change_24h': percent_change_24h,
-                    'logo': logoUrl
-                })
-            })
-        }
-        res.json({
-            trendinglist
+    const cachedData = await redisClient.get('trendinglist');
+    if (cachedData){
+        res.json(JSON.parse(cachedData))
+    } else {
+        await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?convert=KES&limit=5', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
+            }
         })
-    })
+        .then(response => response.json())
+        .then(async(data) => {
+            for (let i = 0; i < data.data.length; i++){
+                const item = data.data[i];
+                const { id, name, symbol } = item;
+                const {price, percent_change_24h} = item.quote.KES;
+                await fetch(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=${id}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
+                    }
+                })
+                .then(response2 => response2.json())
+                .then(data2 => {
+                    let logoUrl = data2.data[id].logo;
+                    trendinglist.push({
+                        'name': name,
+                        'symbol': symbol,
+                        'price': price,
+                        'percent_change_24h': percent_change_24h,
+                        'logo': logoUrl
+                    })
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+            }
+            await redisClient.set('trendinglist', JSON.stringify(trendinglist), 1800);
+            res.json({
+                trendinglist
+            })
+        })
+        .catch(err => {
+            console.error(err);
+        })
+    }
 });
 
 router.get('/crypto/recents', async(req, res) => {
-    await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=5&convert=KES&sort=date_added', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
-        }
-    })
-    .then(response => response.json())
-    .then(async(data) => {
-        const recents = [];
-        for (let i = 0; i < data.data.length; i++){
-            const item = data.data[i];
-            const { id, name, symbol } = item;
-            const { price, percent_change_24h } = item.quote.KES;
-            await fetch(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=${id}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
-                }
-            })
-            .then(response2 => response2.json())
-            .then(data2 => {
-                let logoUrl = data2.data[id].logo;
-                recents.push({
-                    'name': name,
-                    'symbol': symbol,
-                    'price': price,
-                    'percent_change_24h': percent_change_24h,
-                    'logo': logoUrl
-                })
-            })
-            .catch(err => {
-                console.error(err);
-            }) 
-        }
-        res.json({
-            recents
+    const recents = [];
+    const cachedData = await redisClient.get('recents');
+    if (cachedData) {
+        res.json(JSON.parse(cachedData));
+    } else {
+        await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=5&convert=KES&sort=date_added', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
+            }
         })
-    })
-    .catch(err => {
-        console.error(err)
-    })
-});
-
-router.get('/crypto/gainers', async(req, res) => {
-    await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=5&convert=KES&sort=percent_change_24h', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
-        }
-    })
-        .then((response) => response.json())
-        .then(async(data ) => {
-            const gainers = [];
+        .then(response => response.json())
+        .then(async(data) => {
             for (let i = 0; i < data.data.length; i++){
                 const item = data.data[i];
                 const { id, name, symbol } = item;
@@ -155,7 +126,7 @@ router.get('/crypto/gainers', async(req, res) => {
                 .then(response2 => response2.json())
                 .then(data2 => {
                     let logoUrl = data2.data[id].logo;
-                    gainers.push({
+                    recents.push({
                         'name': name,
                         'symbol': symbol,
                         'price': price,
@@ -163,13 +134,28 @@ router.get('/crypto/gainers', async(req, res) => {
                         'logo': logoUrl
                     })
                 })
+                .catch(err => {
+                    console.error(err);
+                }) 
             }
-            res.json({gainers})
+            await redisClient.set('recents', JSON.stringify(recents), 1800)
+            res.json({
+                recents
+            })
         })
-    });
+        .catch(err => {
+            console.error(err)
+        })
+    }
+});
 
-    router.get('/crypto/market-cap', async(req, res) => {
-        await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=5&convert=KES&sort=market_cap', {
+router.get('/crypto/gainers', async(req, res) => {
+    const gainers = [];
+    const cachedData = await redisClient.get('gainers');
+    if (cachedData) {
+        res.json(JSON.parse(cachedData));
+    } else {
+        await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=5&convert=KES&sort=percent_change_24h', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -177,8 +163,7 @@ router.get('/crypto/gainers', async(req, res) => {
             }
         })
             .then((response) => response.json())
-            .then(async(data) => {
-                const marketcap = [];
+            .then(async(data ) => {
                 for (let i = 0; i < data.data.length; i++){
                     const item = data.data[i];
                     const { id, name, symbol } = item;
@@ -193,17 +178,75 @@ router.get('/crypto/gainers', async(req, res) => {
                     .then(response2 => response2.json())
                     .then(data2 => {
                         let logoUrl = data2.data[id].logo;
-                        marketcap.push({
+                        gainers.push({
                             'name': name,
                             'symbol': symbol,
                             'price': price,
                             'percent_change_24h': percent_change_24h,
                             'logo': logoUrl
                         })
-                    })  
+                    })
+                    .catch(err => {
+                        console.error(err)
+                    })
                 }
-                res.json({marketcap})
+                await redisClient.set('gainers', JSON.stringify(gainers), 1800)
+                res.json({gainers})
             })
+            .catch(err => {
+                console.error(err)
+            })
+        }
+    });
+
+    router.get('/crypto/market-cap', async(req, res) => {
+        const marketcap = [];
+        const cachedData = await redisClient.get('marketcap');
+        if (cachedData) {
+            res.json(JSON.parse(cachedData));
+        } else {
+            await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=5&convert=KES&sort=market_cap', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
+                }
+            })
+                .then((response) => response.json())
+                .then(async(data) => {
+                    for (let i = 0; i < data.data.length; i++){
+                        const item = data.data[i];
+                        const { id, name, symbol } = item;
+                        const { price, percent_change_24h } = item.quote.KES;
+                            await fetch(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=${id}`, {
+                            method: 'GET',
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
+                            }
+                        })
+                        .then(response2 => response2.json())
+                        .then(data2 => {
+                            let logoUrl = data2.data[id].logo;
+                            marketcap.push({
+                                'name': name,
+                                'symbol': symbol,
+                                'price': price,
+                                'percent_change_24h': percent_change_24h,
+                                'logo': logoUrl
+                            })
+                        })
+                        .catch(err => {
+                            console.error(err)
+                        })  
+                    }
+                    await redisClient.set('marketcap', JSON.stringify(marketcap), 1800);
+                    res.json({marketcap})
+                })
+                .catch(err => {
+                    console.error(err)
+                })
+            }
         });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const router = require('./routes/index.js');
+const connDB = require('./utils/redis.js');
 
 const app = express();
 
-app.use(cors(
-    {
-        origin: 'http://localhost:3000',
-    }
-))
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With', 'Content-Type');
+
+    next();
+})
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(router);
+
 
 
 app.listen(process.env.PORT || 8000, () => {
